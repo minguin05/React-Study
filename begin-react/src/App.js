f56f973ca646471b65f1c2b02d1886a54e90a939
minguin05 <sjrndnfl@naver.com>
2021-02-03 20:57:34 +0900

    style과 className 예제

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -3,10 +3,21 @@ import './App.css';
 import Hello from './Hello';
 
 function App() {
+  const name = 'react';
+  const style = {
+    background: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem' 
+  }
+
+  // Fragment - 이름 없는 태그, 브라우저 상에서 별도의 엘리먼트로 나타나지 않음 
+  
   return (
-    <div>
-      <Hello />
-    </div>
+    <> 
+      <Hello /> 
+      <div style={style}>{name}</div>
+    </>
   );
 }
 
