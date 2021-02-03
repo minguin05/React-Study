c8ed23628dfbdf951bd8f28648f21153dc5df5af
minguin05 <sjrndnfl@naver.com>
2021-02-03 21:05:56 +0900

    props 사용 예제

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -3,22 +3,10 @@ import './App.css';
 import Hello from './Hello';
 
 function App() {
-  const name = 'react';
-  const style = {
-    background: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem' 
-  }
-
-  // Fragment - 이름 없는 태그, 브라우저 상에서 별도의 엘리먼트로 나타나지 않음 
+  
   
   return (
-    <> 
-      <Hello /> 
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    <Hello name="react"/>
   );
 }
 
