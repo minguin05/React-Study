ab38a3e2f0135d4139c3cd101eab2e4c17bec670
minguin05 <sjrndnfl@naver.com>
2021-02-06 19:04:25 +0900

    useRef 예제

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,22 +1,49 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 
 function InputSample(){
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    });
+    const nameInput = useRef();
+
+    const {name, nickname } = inputs; // 비구조화 할당을 통해 추출
 
     const onChange = (e) => {
-        setText(e.target.value);
+        const {value, name} = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value // name 키를 가진 값을 value로 설정
+        })
     };
 
     const onReset = () => {
-        setText('');
+        setInputs({
+            name : '',
+            nickname: ''
+        });
+        nameInput.current.focus();
     };
 
     return (
         <div>
-            <input onChange={onChange} value={text}/>
+            <input 
+                name = "name" 
+                placeholder="이름" 
+                onChange={onChange} 
+                value = {name}
+                ref={nameInput}
+            />
+            <input 
+                name = "nickname" 
+                placeholder="닉네임" 
+                onChange={onChange} 
+                value={nickname}
+            />
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값: {text}</b>
+                <b>값: </b>
+                {name} ({nickname})
             </div>
         </div>
     );
