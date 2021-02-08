46a93c8b32befcbc09e15f7704b0acd94bad379b
minguin05 <sjrndnfl@naver.com>
2021-02-08 20:47:57 +0900

    배열 삭제 및 수정 예제

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,42 +1,40 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
-import CreateUser from "./CreatUser";
-
+import CreateUser from './CreatUser';
 
 function App() {
   const [inputs, setInputs] = useState({
     username: '',
     email: ''
-  })
-
-  const {username, email} = inputs;
-
+  });
+  const { username, email } = inputs;
   const onChange = e => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     setInputs({
       ...inputs,
       [name]: value
     });
   };
-
   const [users, setUsers] = useState([
     {
       id: 1,
-      username: '민진',
-      email: 'sjrndnfl@naver.com'
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
     },
     {
       id: 2,
       username: 'tester',
-      email: 'teeter@gmail.com'
+      email: 'tester@example.com',
+      active: false
     },
     {
       id: 3,
       username: 'liz',
-      email: 'liz@example.com'
+      email: 'liz@example.com',
+      active: false
     }
   ]);
-  
 
   const nextId = useRef(4);
   const onCreate = () => {
@@ -49,9 +47,23 @@ function App() {
 
     setInputs({
       username: '',
-      email:''
+      email: ''
     });
     nextId.current += 1;
+  };
+
+  const onRemove = id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  };
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
   };
 
   return (
@@ -62,10 +74,10 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
-    
   );
 }
+
 
 export default App;
